3baadecabf8c462c3c894d6effb1c34b309da327
CSirk <csirk005@gmail.com>
2016-09-16 18:58:03 -0500

    test project created

---
+++ CSCodeCore.Test/CSLoggerTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using CSLogger.Extension_Classes;
+using System.IO;
+using System;
+
+namespace CSCodeCore.Test
+{
+    [TestFixture]
+    public class CSLoggerTest
+    {
+        [Test]
+        public void VerifyBooleanExtension_WriteToConsole()
+        {
+        }
+    }
+}
