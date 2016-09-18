0df5e034666fe928e2bca1e7cd034176b683f43c
CSirk <csirk005@gmail.com>
2016-09-17 20:12:06 -0500

    double extensions and test

--- CSCodeCore.Test/CSLoggerTest.cs
+++ CSCodeCore.Test/CSLoggerTest.cs
@@ -2,6 +2,7 @@
 using CSLogger.Extension_Classes;
 using System.IO;
 using System;
+using System.Collections.Generic;
 
 namespace CSCodeCore.Test
 {
@@ -11,6 +12,39 @@ namespace CSCodeCore.Test
         [Test]
         public void VerifyBooleanExtension_WriteToConsole()
         {
+            var isTrue = true;
+            isTrue.WriteToConsole();
+        }
+
+        [Test]
+        public void VerifyIntegerExtension_WriteToConsole()
+        {
+            var i = 10;
+            i.WriteToConsole();
+        }
+
+        [Test]
+        public void VerifyStringExtension_WriteToConsole()
+        {
+            var s = "string";
+            s.WriteToConsole();
+        }
+
+        [Test]
+        public void VerifyDoubleExtension_WriteToConsole()
+        {
+            var test = 0.0;
+            test.WriteToConsole();
+        }
+
+        [Test]
+        public void VerifyEnumerableExtension_WriteToConsole()
+        {
+            var listOfInt = new List<int>{ 1, 2, 3, 4, 5, 6 };
+            var listOfString = new List<string> { "a", "c", "d" };
+
+            listOfInt.WriteToConsole();
+            listOfString.WriteToConsole();
         }
     }
 }
