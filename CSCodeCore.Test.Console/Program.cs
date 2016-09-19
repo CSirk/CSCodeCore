364e1d0c9c1d2a8b802aed36b7c97d313125a9c4
CSirk <csirk005@gmail.com>
2016-09-19 06:40:54 -0500

    added console tests

--- CSCodeCore.Test.Console/Program.cs
+++ CSCodeCore.Test.Console/Program.cs
@@ -13,18 +13,45 @@ namespace CSCodeCore.Test.Console
     {
         static void Main(string[] args)
         {
+            #region Integer Tests
             var test1 = 1;
-            var test2 = "two";
-            var test3 = true;
 
-            var listOfTest4 = new List<double> { 1.0, 2.0, 3.0, 4.0, 5.0 };
-
+            test1.AddToErrorLog(nameof(test1));
+            test1.AddToInfoLog(nameof(test1));
+            test1.WriteToConsole();
             test1.WriteToConsole(nameof(test1));
+
+            #endregion
+
+            #region Boolean Tests
+            bool test2 = true;
+            test2.AddToErrorLog(nameof(test2));
+            test2.AddToInfoLog(nameof(test2));
             test2.WriteToConsole();
+            test2.WriteToConsole(nameof(test2));
+            #endregion
+
+            #region String Tests
+            string test3 = "test3";
             test3.AddToErrorLog(nameof(test3));
             test3.AddToInfoLog(nameof(test3));
+            test3.WriteToConsole();
+            test3.WriteToConsole(nameof(test3));
+            #endregion
 
-            listOfTest4.WriteToConsole();
+            #region Double Tests
+            double test4 = 4.0;
+            test4.AddToErrorLog(nameof(test4));
+            test4.AddToInfoLog(nameof(test4));
+            test4.WriteToConsole();
+            test4.WriteToConsole(nameof(test4));
+            #endregion
+
+            #region Enumerable Tests
+            var test5 = new List<double> { 1.0, 2.0, 3.0 };
+            test5.WriteToConsole();
+            test5.WriteToConsole(nameof(test5));
+            #endregion
 
             ConsoleLogger.PrintErrorLog();
             ConsoleLogger.PrintInfoLog();
