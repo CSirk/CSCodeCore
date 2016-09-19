71762b07f918a5e32c4af91258648de26de8db54
CSirk <csirk005@gmail.com>
2016-09-18 21:51:25 -0500

    added console test project

---
+++ CSCodeCore.Test.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSLogger;
+using CSLogger.Extension_Classes;
+
+namespace CSCodeCore.Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = 1;
+            var test2 = "two";
+            var test3 = true;
+
+            var listOfTest4 = new List<double> { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+            test1.WriteToConsole(nameof(test1));
+            test2.WriteToConsole();
+            test3.AddToErrorLog(nameof(test3));
+
+            listOfTest4.WriteToConsole();
+
+           System.Console.ReadKey();
+        }
+    }
+}
---
+++ CSLogger/Extension_Classes/ILogableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CSLogger.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLogger.Extension_Classes
+{
+    public static class ILogableExtensions
+    {
+        public static void WriteToConsole(this ILogable classToWrite)
+        {
+
+
+        }
+    }
+}
